1ae04e54e4e967f5ad4c0df71e3d424b6854337a
Diya Yunus <dyunus2@illinois.edu>
2022-02-28 16:34:02 -0600

    Slight fixes to user database

--- Flask/flaskr/__init__.py
+++ Flask/flaskr/__init__.py
@@ -4,7 +4,9 @@ from flask import Flask
 
 # $ export FLASK_APP=flaskr
 # $ export FLASK_ENV=development
+# $ flask init db
 # $ flask run
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -14,19 +16,17 @@ def create_app(test_config=None):
     )
 
     if test_config is None:
-        # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
-        # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
+
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-    # a simple page that says hello
+    
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
